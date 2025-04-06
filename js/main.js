dcd3372e5bb07e0b27a0fd0b93b809b3e95ec583
ryo-takehira <145681998+ryo-takehira@users.noreply.github.com>
2025-04-06 18:01:37 +0900

    修正

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-// ハンバーガーメニュー
 var hamburger = $('.hamburger-menu');
 
-// ハンバーガーメニューボタンがクリックされたとき
 $('.hamburger-button').on('click', function () {
     hamburger.slideToggle(500);
 });
@@ -9,14 +7,11 @@ $('.hamburger-button').on('click', function () {
 
 $(window).on('resize', function () {
     if ($(window).width() < 768) {
-        hamburger.removeAttr('style'); // スライドで設定されたdisplayをリセット
+        hamburger.removeAttr('style');
     }
 });
 
 
-
-
-// カルーセル
 $('.cover-slick').slick({
     arrows: false,
     adaptiveHeight: false,
@@ -26,27 +21,20 @@ let img = document.querySelector('img');
 console.log(img.naturalWidth, img.naturalHeight);
 
 
-
-
-
 $('.feature-image').waypoint({
     handler: function (direction) {
         if (direction === 'down') {
-            // フェードインアップというクラスを付与してます
             $(this.element).addClass('animate__fadeInUp');
-
-            // アニメーションが１回しか呼ばれないようにするためにdestroyしてます
             this.destroy();
         }
     },
-
-    // 画面中央に来たらhandlerを実行する設定です
     offset: '50%',
 });
 
-$('.click-open dt').on('click', function (e) {
 
-    $(this).toggleClass('open');
-    $(this).next().slideToggle();
+$('.click-open dt').on('click', function (e) {
+    let $dl = $(this).closest('.click-open');
+    $dl.toggleClass('open'); // dl に open クラスを付与
+    $(this).next().slideToggle(); // 次の dd を開閉
 
 });
